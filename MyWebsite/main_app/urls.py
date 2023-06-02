bb2bc0214ea9a500b4107ee119dd4ae0a25285d6
Yousef M <yousefmalsaadan@gmail.com>
2023-06-02 23:20:46 +0300

    home page

--- MyWebsite/main_app/urls.py
+++ MyWebsite/main_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'main_app'
 
 urlpatterns = [
-    path('', views.home_page, name = 'home_page')
+    path('', views.home_page, name = 'home_page'),
+    path('about/', views.about_page, name = 'about_page'),
+    path('projects/', views.projects_page, name = 'projects_page'),
+    path('contact/', views.contact_page, name = 'contact_page')
 ]
--- MyWebsite/main_app/views.py
+++ MyWebsite/main_app/views.py
@@ -4,3 +4,15 @@ from django.http import HttpRequest
 # Create your views here.
 def home_page(request: HttpRequest):
     return render(request, 'main_app/home.html')
+
+
+def about_page(request: HttpRequest):
+    return render(request, 'main_app/about.html')
+
+
+def projects_page(request: HttpRequest):
+    return render(request, 'main_app/project.html')
+
+
+def contact_page(request: HttpRequest):
+    return render(request, 'main_app/contact.html')
