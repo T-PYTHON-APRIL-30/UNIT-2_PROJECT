53e8724f980444b2fd2c641618b7e12f50c19b79
NouraAldayhani <nouraa.sultan9@gmail.com>
2023-06-05 22:02:51 +0300

    Version1 home_page almost done.

---
+++ PersonalWebsite/main_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render,redirect
+from django.http import HttpRequest, HttpResponse
+# Create your views here.
+
+def home_page(request:HttpRequest):
+    return render(request, "main_app/home_page.html")
