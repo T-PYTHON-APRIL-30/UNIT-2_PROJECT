c9c0b2114b079d51c15b0ae645981c869f6a2504
lena <lenaashayban@gmail.com>
2023-06-05 14:41:10 +0300

    project-unit2

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-def contact
+class contact(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    message = models.TextField()
