45b0819927e15bc2d01192faaa82cd6930cf9070
Bakr Hawsawi <122125253+Bakr40@users.noreply.github.com>
2023-06-05 22:37:58 +0300

    commit

--- minProject/profile_app/views.py
+++ minProject/profile_app/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpRequest,HttpResponse
-
+from django.http import HttpRequest, HttpResponse
+from .models import Contact
 # Create your views here.
 
-def index_page(request:HttpRequest):
 
-    return render(request,"profile_app/index.html")
+def index_page(request: HttpRequest):
 
-def about_page(request:HttpRequest):
+    return render(request, "profile_app/index.html")
 
-    return render(request,"profile_app/about.html")
 
-def skill_page(request:HttpRequest):
+def about_page(request: HttpRequest):
 
-    return render(request,"profile_app/skills.html")
+    return render(request, "profile_app/about.html")
 
-def contact_page(request:HttpRequest):
 
-    return render(request,"profile_app/contact.html")
+def skill_page(request: HttpRequest):
 
-def project_page(request:HttpRequest):
+    return render(request, "profile_app/skills.html")
 
-    return render(request,"profile_app/project.html")
+
+def contact_page(request: HttpRequest):
+
+    if request.method == "POST":
+        new_contact = Contact(first_name=request.POST["first_name"], last_name=request.POST["last_name"],
+                              email=request.POST["email"], mobile=request.POST["mobile"], message=request.POST["message"])
+        new_contact.save()
+
+    return render(request, "profile_app/contact.html")
+
+
+def project_page(request: HttpRequest):
+
+    return render(request, "profile_app/project.html")
