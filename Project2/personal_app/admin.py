d39f073126047c971cec5ab259ebcfff2797eaa6
Dalia <daliaasq@gmail.com>
2023-06-06 20:09:14 +0300

    Tuesday version2

--- Project2/personal_app/admin.py
+++ Project2/personal_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Project, contact
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title','date',)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name','email','send_at')
+
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(contact,ContactAdmin)
--- Project2/personal_app/models.py
+++ Project2/personal_app/models.py
@@ -8,8 +8,14 @@ class contact(models.Model):
     message = models.TextField()
     send_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return f"Message from {self.name}"
+
 class Project(models.Model):
     title = models.CharField(max_length=2048)
     content = models.TextField()
     date = models.DateTimeField()
     image = models.ImageField(upload_to="images/", default="images/default.png")
+
+    def __str__(self) -> str:
+        return f"{self.title}"
