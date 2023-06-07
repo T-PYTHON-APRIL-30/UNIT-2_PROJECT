252770fd550919dc0398b3a2efdcc74015651872
NouraAldayhani <nouraa.sultan9@gmail.com>
2023-06-07 03:09:28 +0300

    Version2 almost done!

--- PersonalWebsite/main_app/urls.py
+++ PersonalWebsite/main_app/urls.py
@@ -5,5 +5,13 @@ app_name= "main_app"
 
 urlpatterns=[
     path('',views.home_page ,name="home_page"),
+    path('add/project/',views.add_project,name="add_project"),
+    path("delete/<project_id>",views.delete_project,name="delete_project"),
+    path('project/<project_id>/detail/',views.project_detail ,name="project_detail"),
+    path('read/message/',views.read_message,name="read_message"),
+    path('add/message/',views.add_message,name="add_message"),
+    path('callOfduty/game/',views.game_one,name="game_one"),
+    path('overwatch/game/',views.game_two,name="game_two"),
+    path('rocket/game/',views.game_three,name="game_three"),
 
 ]
