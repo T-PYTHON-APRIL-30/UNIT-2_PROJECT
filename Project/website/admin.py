cf147c61cb260a988a52a9ce1296f12930add537
Omar Ali <Omar.ali99@live.com>
2023-06-07 11:37:14 +0300

    push after presentation

--- Project/website/admin.py
+++ Project/website/admin.py
@@ -10,14 +10,14 @@ class TitleAdmin(admin.ModelAdmin):
 
 class NameAdmin(admin.ModelAdmin):
     list_filter = ('name',)
-    list_display = ('name',)
+    list_display = ('name', 'Novel')
 
 class FullnameAdmin(admin.ModelAdmin):
     list_filter = ('fullname',)    
     list_display = ('fullname',)
 
 
-admin.site.register(novel,TitleAdmin)
+admin.site.register(novel, TitleAdmin)
 admin.site.register(comment, NameAdmin)
 admin.site.register(inquiry, FullnameAdmin)
 
